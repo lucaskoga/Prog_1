d5dc3c664315d021d8b8b1516a6314d0a052aabd
Lucas Koga <54293587+lucaskoga@users.noreply.github.com>
2021-09-18 00:05:55 -0300

    Create Multiplicacao.c

---
+++ Atividade Lista4/Multiplicacao/Multiplicacao.c
@@ -0,0 +1,26 @@
+/*
+3. Faca um programa que receba como entrada dois nu ́meros inteiros e realize a multiplicação deles
+sem utilizar o operador de *.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Declaracao de variavel 
+    int num1,num2,count,mult;
+    //Entrada pelo teclado 
+    printf("Qual o primeiro numero da multiplicacao: ");
+    scanf("%d",&num1);
+    printf("Qual o segundo numero da multiplicacao: ");
+    scanf("%d",&num2);
+    //Inicia a variavel que guarda  a multiplicacao em 0
+    mult = 0;
+    //Faz a repeticao até alcancar o valor de count igual a num2
+    while(count < num2){
+         mult = mult + num1;
+         count++;
+    }
+    printf("A multiplicacao entre %d e %d e igual a %d: ",num1 ,num2, mult);
+    return 0;
+}
