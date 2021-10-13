451ae3509d244a16d456352c5a0267dc90af0462
Lucas Koga <54293587+lucaskoga@users.noreply.github.com>
2021-10-13 10:57:46 -0300

    Update Pesquisa.c

--- 1Prova Avaliativa/Questao 2/Pesquisa.c
+++ 1Prova Avaliativa/Questao 2/Pesquisa.c
@@ -1,3 +1,13 @@
+/*
+2. [2,0] Uma pesquisa foi realizada com habitantes da regiao do Norte Pioneiro do Estado
+do Parana. Na pesquisa, foram coletados dados como sexo (1 – feminino, 0 – masculino)
+e salario. Sua tarefa ́e implementar um programa que receba os dados dos profissionais
+(maximo 100), calcule e mostre:
+(a) A m ́edia salarial da populacao;
+(b) O maior salario encontrado;
+(c) A media salarial das mulheres;
+(d) O percentual de mulheres na populacao.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,12 +16,11 @@ int main()
     //Declaro as variaveis
     int i, n, count, x;
     float salario, salarioTotal, maior, menor, mulher, porcentualMulher;
-  
     //inicio o valores
     maior = salario;
     count = 0;
     x = 100;
-  
+
     //inicio o for
     for( i = 0; i < x; i++){
         //Pegos as entrada pelo teclado
